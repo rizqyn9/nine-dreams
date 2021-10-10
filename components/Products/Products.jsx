0bf034ef7f2d7621ec023a188957409c2d7ac0d7
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2021-10-10 17:38:11 +0700

    feat: products

---
+++ components/Products/Products.jsx
@@ -0,0 +1,38 @@
+export default function Products(){
+    return (
+        <section className="products">
+            <div className="content-container">
+                <div className="top-products">
+                    <div className="title text-lg">
+                        Top Products
+                    </div>
+                </div>
+                <div className="main-content">
+                    <ListContainer/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function ListContainer() {
+    return (
+        <div className="list-container">
+            <ProductItem/>
+            <ProductItem/>
+            <ProductItem/>
+            <ProductItem/>
+            <ProductItem/>
+            <ProductItem/>
+            <ProductItem/>
+        </div>
+    )
+}
+
+function ProductItem() {
+    return (
+        <div className="product-item">
+
+        </div>
+    )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Nav from "../components/Navbar/Nav"
 import HomePage from "../components/HomePage/HomePage"
+import Products from "../components/Products/Products"
 
 export default function Home() {
   return(
@@ -7,6 +8,7 @@ export default function Home() {
       <Nav/>
       <main>
         <HomePage/>
+        <Products/>
       </main>
     </>
   )
