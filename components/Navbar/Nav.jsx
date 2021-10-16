aefbf57288d5945db180672313e9fb348b5facad
rizqyn9 <54632504+rizqyn9@users.noreply.github.com>
2021-10-16 19:49:09 +0700

    feat: nav mobile animation

--- components/Navbar/Nav.jsx
+++ components/Navbar/Nav.jsx
@@ -1,11 +1,13 @@
 import {useEffect, useState, useRef} from 'react';
-import {motion} from "framer-motion"
+import {motion, useAnimation} from "framer-motion"
+import {useAtom, atom} from "jotai"
 
 export default function Nav (){
     const [navActive, setNavActive] = useState(false);
 
     function handleNavBtn() {
         setNavActive(!navActive)
+        console.log("asdad");
     }
 
     return (
@@ -23,15 +25,87 @@ export default function Nav (){
         >
             <div className="logo">ninedreams</div>
             <div className={`btn-nav${navActive? "-active" : ""}`} onClick={handleNavBtn}>|||</div>
-            <NavMenu active={navActive}/>
+            <NavContainerMobile  active={navActive}/>
+            <NavMenu/>
         </motion.nav>
     )
 }
 
-function NavMenu(props) {
+function NavContainerMobile(props) {
     const {active} = props;
+    const animController = useAnimation();
+
+    const variant = {
+        open : {
+            opacity:1,
+            x:"0"
+        },
+        close: {
+            opacity:0,
+            x:"-100vw"    
+        }
+    }
+
+    useEffect( async () => {
+        if(active){
+            await animController.start("open")
+
+        } else {
+            animController.start("close")
+        }
+    }, [active])
+
+    return(
+        <motion.div className={`menu-container-mobile nav-active`}
+            variants={variant}
+            initial="closed"
+            animate={animController}
+            transition={{
+                duration:.5,
+                staggerChildren:.1,
+                staggerDirection : active ? -1 : 1,
+                when: active ? "beforeChildren" : "afterChildren"
+            }}
+        >
+            <MenuItemMobile name="home"/>
+            <MenuItemMobile name="products"/>
+            <MenuItemMobile name="creative communication"/>
+            <MenuItemMobile name="about"/>
+            <MenuItemMobile name="contact"/>
+        </motion.div>
+    )
+}
+
+function MenuItemMobile(props) {
+    const {name } = props;
+
+    const variant = {
+        open: {
+            x: "0",
+            opacity: 1,
+        },
+        close: {
+            x: "-100vw"
+        }
+    }
+
+    return(
+        <motion.div className='menu-mobile'
+            variants={variant}
+            transition={{
+                duration:.3
+            }}
+        >
+            {name}
+        </motion.div>
+    )
+}
+
+function NavMenu(props) {
+
     return (
-        <motion.div className={`menu-container ${active ? "menu-active" : ""}`}>
+        <motion.div className={`menu-container`}
+        >
             <MenuItem name="home"/>
             <MenuItem name="products"/>
             <MenuItem name="creative communication"/>
@@ -44,8 +118,8 @@ function NavMenu(props) {
 function MenuItem(props){
     const {name} = props;
     return (
-        <div className="menu">
+        <motion.div className="menu">
             {name}
-        </div>
+        </motion.div>
     )
 }
