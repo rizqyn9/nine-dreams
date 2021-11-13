36fd0b92910350762b64a1ff6b5573e1c7d23d67
Rizqy Prastya Ari Nugroho <54632504+rizqyn9@users.noreply.github.com>
2021-11-13 23:42:38 +0700

    feat: facebook verification

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         /> */}
+        <meta name="facebook-domain-verification" content="c6y4ies041w9x9xdxqiuegcf3uqa2c" />
         <title>ninedreams</title>
       </Head>
       <Component {...pageProps} />
